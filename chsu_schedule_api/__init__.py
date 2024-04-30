afdd0a9575c8c4f3b70f03b3b89d8c78af9e28f9
VoVcHiC <tsvetkovvova17@gmail.com>
2024-04-30 15:57:50 +0300

    __all__ sort

--- chsu_schedule_api/__init__.py
+++ chsu_schedule_api/__init__.py
@@ -1,4 +1,4 @@
 from .api import ABCApi, CHSUApi
 from .client import ABCHttpClient, AiohttpClient
 
-__all__ = ("ABCApi", "CHSUApi", "ABCHttpClient", "AiohttpClient")
+__all__ = ("ABCApi", "ABCHttpClient", "AiohttpClient", "CHSUApi")
