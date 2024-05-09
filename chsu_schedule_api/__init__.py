692abc9cf5a7b2391c25750ae94baacaa1b24ba6
VoVcHiC <tsvetkovvova17@gmail.com>
2024-05-10 02:23:56 +0300

    syncify async methods

--- chsu_schedule_api/__init__.py
+++ chsu_schedule_api/__init__.py
@@ -1,4 +1,6 @@
-from .api import ABCApi, CHSUApi
+from chsu_schedule_api.sync import CHSUApi
+
+from .api import ABCApi
 from .client import ABCHttpClient, AiohttpClient
 
 __all__ = ("ABCApi", "ABCHttpClient", "AiohttpClient", "CHSUApi")
---
+++ chsu_schedule_api/sync.py
@@ -0,0 +1,34 @@
+import asyncio
+import functools
+import inspect
+
+from .api import CHSUApi
+
+
+def async_to_sync(obj: object, name: str) -> None:
+    method = getattr(obj, name)
+
+    @functools.wraps(method)
+    def sync_wrapper(*args, **kwargs) -> object:
+        coro = method(*args, **kwargs)
+        try:
+            loop = asyncio.get_event_loop()
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+        if loop.is_running():
+            return coro
+        return loop.run_until_complete(coro)
+
+    setattr(obj, name, sync_wrapper)
+
+
+def syncify(obj: object) -> None:
+    for name in dir(obj):
+        if not name.startswith("_") and inspect.iscoroutinefunction(
+            getattr(obj, name)
+        ):
+            async_to_sync(obj, name)
+
+
+syncify(CHSUApi)
