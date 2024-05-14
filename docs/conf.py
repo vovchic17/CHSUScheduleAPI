9c29a6765147847f27da767d5c1613eddfbcf0e1
VoVcHiC <tsvetkovvova17@gmail.com>
2024-05-14 15:54:07 +0300

    add opengraph

--- docs/conf.py
+++ docs/conf.py
@@ -1,26 +1,31 @@
-import os  # noqa: INP001
+import os
 import sys
 from datetime import date
 
-sys.path.insert(0, os.path.abspath(".."))  # noqa: PTH100
+sys.path.insert(0, os.path.abspath(".."))
 project = "CHSUScheduleAPI"
 author = "VoVcHiC"
-copyright = f"{date.today().year}, {author}"  # noqa: DTZ011, A001
+copyright = f"{date.today().year}, {author}"
 
 templates_path = ["_templates"]
 html_theme = "furo"
-html_logo = (
-    "https://raw.githubusercontent.com/vovchic17/static/main/src/logo.svg"
-)
+html_logo = "_static/logo.png"
 html_css_files = ["extra.css"]
 html_js_files = ["theme_modes.js"]
 html_static_path = ["_static"]
 todo_include_todos = True
 html_show_sourcelink = False
+
+ogp_site_url = "https://docs.chsutech.ru"
+ogp_site_name = "CHSUScheduleAPI documentation"
+ogp_description_length = 0
+ogp_social_cards = {"image_mini": "_static/rtd.ico", "description": "test"}
+
 extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx_copybutton",
     "sphinx.ext.todo",
+    "sphinxext.opengraph",
 ]
